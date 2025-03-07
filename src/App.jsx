ee063809273ce8023bbdab14dc6ce4fb6d31dff7
Shubham <shubham9.sy@gmail.com>
2025-03-07 23:12:22 +0530

    Fixed GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 // Component import
 import Navbar from "./components/Navbar/Navbar";
